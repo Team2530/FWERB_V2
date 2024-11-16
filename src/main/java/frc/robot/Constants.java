b2be250a68ae0423491709d292b70217addfe130
NifleySnifley <finnsbart@gmail.com>
2024-11-15 19:01:34 -0600

    Calibration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -93,7 +93,7 @@ public final class Constants {
     public static final int FL_DRIVE_ID = 4;
     public static final int FL_STEER_ID = 4;
     public static final int FL_ABSOLUTE_ENCODER_PORT = 4;
-    public static final double FL_OFFSET_RADIANS = Units.rotationsToRadians(0.388428) + Math.PI * 0.5 + Math.PI;
+    public static final double FL_OFFSET_RADIANS = Units.rotationsToRadians(0.389893) + Math.PI * 0.5 + Math.PI;
     public static final boolean FL_ABSOLUTE_ENCODER_REVERSED = true;
     public static final boolean FL_MOTOR_REVERSED = true;
 
@@ -101,7 +101,7 @@ public final class Constants {
     public static final int FR_DRIVE_ID = 1;
     public static final int FR_STEER_ID = 1;
     public static final int FR_ABSOLUTE_ENCODER_PORT = 1;
-    public static final double FR_OFFSET_RADIANS = Units.rotationsToRadians(0.324219) + Math.PI * 0.5 + Math.PI;
+    public static final double FR_OFFSET_RADIANS = Units.rotationsToRadians(0.323730) + Math.PI * 0.5 + Math.PI;
     public static final boolean FR_ABSOLUTE_ENCODER_REVERSED = true;
     public static final boolean FR_MOTOR_REVERSED = true;
 
@@ -109,7 +109,7 @@ public final class Constants {
     public static final int BR_DRIVE_ID = 2;
     public static final int BR_STEER_ID = 2;
     public static final int BR_ABSOLUTE_ENCODER_PORT = 2;
-    public static final double BR_OFFSET_RADIANS = Units.rotationsToRadians(-0.358398) + Math.PI * 0.5 + Math.PI;
+    public static final double BR_OFFSET_RADIANS = Units.rotationsToRadians(-0.360107) + Math.PI * 0.5 + Math.PI;
     public static final boolean BR_ABSOLUTE_ENCODER_REVERSED = true;
     public static final boolean BR_MOTOR_REVERSED = true;
 
@@ -117,7 +117,7 @@ public final class Constants {
     public static final int BL_DRIVE_ID = 3;
     public static final int BL_STEER_ID = 3;
     public static final int BL_ABSOLUTE_ENCODER_PORT = 3;
-    public static final double BL_OFFSET_RADIANS = Units.rotationsToRadians(0.393799) + Math.PI * 0.5 + Math.PI;
+    public static final double BL_OFFSET_RADIANS = Units.rotationsToRadians(0.399902) + Math.PI * 0.5 + Math.PI;
     public static final boolean BL_ABSOLUTE_ENCODER_REVERSED = true;
     public static final boolean BL_MOTOR_REVERSED = true;
 
